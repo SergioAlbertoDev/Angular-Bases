e98dd0f4817a979b0b9f2f5bcbf4a498c9aad0a4
Sergio Alberto Correa Carrillo <s.a.correa.c@outlook.com>
2024-03-18 18:15:27 -0600

    Tipos Basicos

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+const name = 'Sergio';
+let hpPoint:number = 95;
+let hpPoint2:number | string = 95;
+let hpPoint3:number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+console.log(
+    {
+        name,
+        hpPoint,
+        hpPoint2,
+        hpPoint3,
+        isAlive
+    }
+)
+export {};
